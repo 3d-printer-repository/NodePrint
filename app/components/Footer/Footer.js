c21a6014120038b5bec132dcbbc4eef642f0a57d
CA-CharlKlein <Charl.Klein@CaAfrica.co.za>
2018-05-17 08:27:11 +0200

    Introduced React Boiler Plate

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.scss';
+
+const Footer = () => (
+  <footer>
+    <section>This project is licensed under the MIT license.</section>
+    <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by <a href="https://github.com/CharlKlein">Charl Klein</a></section>
+  </footer>
+);
+
+export default Footer;
